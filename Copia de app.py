04e7851bfee9eb3b45b42f2a6b6a3efcfbb6c19e
OGGO <160192019+oggowav@users.noreply.github.com>
2024-11-12 20:24:40 -0600

    Add files via upload

---
+++ Copia de app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import requests
+from random import randint
+
+app = Flask (__name__)
+
+@app.route('/')
+def index():
+    numero_aleatorio = randint(1, 150)
+    respuesta = requests.get(f"https://pokeapi.co/api/v2/pokemon/{numero_aleatorio}")
+    datos = respuesta.json()
+    return render_template('index.html',pokemon = datos)
+
+if __name__ == '__main__':
+    app.run()
